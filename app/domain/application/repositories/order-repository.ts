c9d5d907e83fbadb0f72c42c93c7090fa28a2e4d
codesofC <inelusjudelin01@gmail.com>
2025-02-02 17:30:01 -0300

    jinelus : #3 : feat create prisma repositories and mappers

--- app/domain/application/repositories/order-repository.ts
+++ app/domain/application/repositories/order-repository.ts
@@ -3,6 +3,6 @@ import { Order } from "../../enterprise/entities/order"
 export interface OrderRepository {
     create(order: Order): Promise<void>
     findById(id: string): Promise<Order | null>
-    findAll(): Promise<Order[]>
+    findOrdersByClient(clientId: string): Promise<Order[]>
     delete(order: Order): Promise<void>
 }
