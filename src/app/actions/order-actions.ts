185efbcd2254a55980971bcc1fb589b77fca0729
codesofC <inelusjudelin01@gmail.com>
2025-02-09 21:14:16 -0300

    jinelus : #11 : (feat) create order server actions

---
+++ src/app/actions/order-actions.ts
@@ -0,0 +1,51 @@
+import { PrismaClientRepository } from "@/src/db/repositories/prisma-client-repository";
+import { PrismaOrderRepository } from "@/src/db/repositories/prisma-order-repository";
+import { CreateOrderUseCase, CreateOrderUseCaseProps } from "@/src/domain/application/use-cases/create-order";
+import { DeleteOrderUseCase } from "@/src/domain/application/use-cases/delete-order";
+import { FetchOrdersByClientUseCase } from "@/src/domain/application/use-cases/fetch-orders-client";
+import { GetOrderUseCase } from "@/src/domain/application/use-cases/get-order";
+
+const orderRepository = new PrismaOrderRepository()
+const clientRepository = new PrismaClientRepository()
+
+export async function createOrder(order: CreateOrderUseCaseProps){
+
+    const {idClient, total} = order
+
+    const createOrderUseCase = new CreateOrderUseCase(orderRepository, clientRepository)
+
+    await createOrderUseCase.execute({idClient, total})
+
+}
+
+export async function getOrder(orderId: string){
+    const getOrderUseCase = new GetOrderUseCase(orderRepository)
+
+    const result = await getOrderUseCase.execute({orderId})
+
+    if(result.isLeft()) {
+        return result.value
+    }
+
+    return result.value.order
+}
+
+export async function fetchOrdersByClient({userId, page = 1}: {userId: string, page?: number}){
+
+    const fetchOrdersByClientUseCase = new FetchOrdersByClientUseCase(orderRepository, clientRepository)
+
+    const result = await fetchOrdersByClientUseCase.execute({clientId: userId, page})
+    
+    return result.value
+}
+
+export async function deleteOrder(orderId: string){
+
+    const deleteOrderUseCase = new DeleteOrderUseCase(orderRepository)
+
+    const result = await deleteOrderUseCase.execute({orderId})
+
+    if(result.isLeft()) {
+        return result.value
+    }
+}
