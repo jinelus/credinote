216e6eaab241f3163fc9631836792187e4dfde23
codesofC <inelusjudelin01@gmail.com>
2025-04-12 20:53:24 -0300

    Ajout d'une nouvelle page Dashboard avec des actions rapides et un tableau des commandes récentes. Création d'une page Clients pour afficher la liste des clients et les détails d'un client sélectionné. Intégration de la gestion des utilisateurs avec NextAuth et ajout d'un fichier d'authentification pour la validation des utilisateurs.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -90,4 +90,7 @@ export const authOptions: AuthOptions = {
 };
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
+
+export const dynamic = 'force-dynamic';
---
+++ src/lib/auth.ts
@@ -0,0 +1,67 @@
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { prisma } from '@/src/db/prisma-service'
+import { compare } from 'bcryptjs'
+
+export const authOptions: NextAuthOptions = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  adapter: PrismaAdapter(prisma) as any,
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email e senha são obrigatórios')
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+
+        if (!user) {
+          throw new Error('Usuário não encontrado')
+        }
+
+        const isPasswordValid = await compare(credentials.password, user.password)
+
+        if (!isPasswordValid) {
+          throw new Error('Senha inválida')
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          role: 'user'
+        }
+      }
+    })
+  ],
+  session: {
+    strategy: 'jwt'
+  },
+  pages: {
+    signIn: '/login'
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+      }
+      return session
+    }
+  }
+} 
