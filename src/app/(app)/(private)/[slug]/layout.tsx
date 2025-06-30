2327ca594a69c35918b0f3a2ec836bddcbd5dced
codesofC <inelusjudelin01@gmail.com>
2025-06-29 23:06:50 -0300

    feat : Ajout de la vérification de l'organisation dans le layout privé, redirection vers la page de connexion si l'organisation n'est pas trouvée. Amélioration de la structure du composant avec un conteneur pour le contenu principal.

--- src/app/(app)/(private)/[slug]/layout.tsx
+++ src/app/(app)/(private)/[slug]/layout.tsx
@@ -1,20 +1,29 @@
+import { getOrganizationBySlug } from "@/src/app/actions/organization";
+import { Container } from "@/src/components/base-components/container";
 import { Navbar } from "@/src/components/navbar";
 import { NavContextProvider } from "@/src/components/navbar/nav-context";
 import { Sidebar } from "@/src/components/sidebar";
+import { redirect } from "next/navigation";
 
 export default async function PrivateRootLayout({ children, params }: { children: React.ReactNode, params: Promise<{ slug: string }> }){
 
     const slug = (await params).slug
 
+    const organization = await getOrganizationBySlug(slug)
+
+    if (!organization) {
+        redirect('/signin')
+    }
+
     return (
-        <main className="h-dvh max-h-dvh overflow-hidden w-full flex">
+        <main className="h-dvh max-h-dvh overflow-hidden w-full flex flex-col">
             <NavContextProvider>
-                <Sidebar slug={slug} />
-                <div className="flex-[5] overflow-y-auto">
-                    <Navbar />
-                    <div className="flex justify-center">
+                <Navbar />
+                <div className="flex flex-1">
+                    <Sidebar slug={slug} />
+                    <Container className="flex justify-center overflow-y-auto">
                         {children}
-                    </div>
+                    </Container>
                 </div>
             </NavContextProvider>
         </main>
