79efcdeebc804ec40e44265a092dcc102d7b8429
codesofC <inelusjudelin01@gmail.com>
2025-12-13 15:00:12 -0300

    #73 : refactor: remove unused 'usePathname' import and simplify SidebarLink component

--- src/components/sidebar/link.tsx
+++ src/components/sidebar/link.tsx
@@ -1,8 +1,5 @@
-'use client'
-
 import type { Route } from 'next'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { SidebarMenuButton } from '../ui/sidebar'
 
 export const SidebarLink = ({
@@ -10,15 +7,8 @@ export const SidebarLink = ({
 }: {
   item: { label: string; href: string; icon: React.ReactNode }
 }) => {
-  const pathname = usePathname()
-
   return (
-    <SidebarMenuButton
-      className="h-10 items-center gap-x-4 px-4"
-      asChild
-      tooltip={item.label}
-      isActive={pathname.startsWith(item.href)}
-    >
+    <SidebarMenuButton className="h-10 items-center gap-x-4 px-4" asChild tooltip={item.label}>
       <Link href={item.href as Route}>
         {item.icon}
         <span className="">{item.label}</span>
